2d48863780d73f8768c3203160d0118f3c995488
Petri Asunmaa <petri.asunmaa@outlook.com>
2024-08-05 14:38:07 +0300

    11.9 another test added

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('navigate to ivysaur page and check for chlorophyll', async ({ page }) => {
+    await page.goto('/')
+    await page.click('text=ivysaur')                            // Click on the link to Ivysaur's page
+    await expect(page).toHaveURL(/.*ivysaur/)                   // Ensure the URL contains 'ivysaur'
+    await expect(page.getByText('chlorophyll')).toBeVisible()   // Check for the presence of 'chlorophyll'
+  })
+
 })
